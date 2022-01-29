a7707aacecb75f8e49f795b968f61f31013b7469
anusha <anuskasthacr7@gmail.com>
2022-01-29 19:52:28 +0545

    message

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle r1= new Rectangle();
+        Rectangle r2 = new Rectangle();
+        r1.area(20,3);
+        r2.perimeter(2,3);
+
+    }
+}
---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class Rectangle {
+
+    void area (float length, float base)
+    {
+        float area=(float)(length * base);
+        System.out.println(area);
+
+    }
+    void perimeter(int length ,int base)
+
+    {
+        int perimeter=2*(length + base);
+        System.out.println(perimeter);
+    }
+
+
+
+}
